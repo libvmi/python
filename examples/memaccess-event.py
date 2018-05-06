6858d9c0b1a0f60f05f2b1de04b054f589f45406
Mathieu Tarral <mathieu.tarral@f-secure.com>
2018-05-07 01:40:23 +0300

    update memaccess-event.py

--- examples/memaccess-event.py
+++ examples/memaccess-event.py
@@ -3,11 +3,11 @@
 
 import sys
 import signal
+from pprint import pprint
 
 from libvmi import Libvmi, INIT_DOMAINNAME, INIT_EVENTS
 from libvmi.event import MemEvent, MemAccess, EventResponse
-
-from pprint import pprint
+from utils import dtb_to_pname
 
 
 # catch SIGINT
@@ -22,12 +22,13 @@ def signal_handler(signal, frame):
 
 
 def callback(vmi, event):
+    pprint(event.to_dict())
     if (event.cffi_event.x86_regs.rip != event.data['symbol_addr']):
         # page hit, but we are not at the right address
         # emulate this instruction and continue
         return EventResponse.EMULATE
-
-    pprint(event.to_dict())
+    pname = dtb_to_pname(vmi, event.cffi_event.x86_regs.cr3)
+    print('At {}: {}'.format(event.data['symbol'], pname))
 
 
 def main(args):
@@ -52,8 +53,7 @@ def main(args):
         page = paddr >> 12
         print("paddr: {}".format(hex(paddr)))
         print("page: {}".format(hex(page)))
-        mem_event = MemEvent(MemAccess.X, page, callback,
-                             data=user_data)
+        mem_event = MemEvent(MemAccess.X, callback, gfn=page, data=user_data)
         vmi.register_event(mem_event)
         # listen
         while not interrupted:
