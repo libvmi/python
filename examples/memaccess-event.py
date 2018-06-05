340a45b0c7d0403d716c735e090e2a6b68cae547
Mathieu Tarral <mathieu.tarral@f-secure.com>
2018-06-05 15:13:41 +0300

    update memaccess-event to add singlestepping instead of emulation

--- examples/memaccess-event.py
+++ examples/memaccess-event.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
+"""Memory Access example.
+
+Usage:
+  memaccess-event.py [options] <vm_name> <symbol>
+
+Options:
+  -h --help     Show this screen.
+  --sstep       Use singlestepping instead of emulation
+"""
 
 import sys
 import signal
 from pprint import pprint
 
+from docopt import docopt
 from libvmi import Libvmi, INIT_DOMAINNAME, INIT_EVENTS
-from libvmi.event import MemEvent, MemAccess, EventResponse
-from utils import dtb_to_pname
+from libvmi.event import MemEvent, MemAccess, SingleStepEvent, EventResponse
+from utils import dtb_to_pname, pause
 
 
 # catch SIGINT
@@ -21,47 +31,77 @@ def signal_handler(signal, frame):
     interrupted = True
 
 
-def callback(vmi, event):
-    pprint(event.to_dict())
-    if (event.cffi_event.x86_regs.rip != event.data['symbol_addr']):
+def cb_mem_event(vmi, event):
+    # pprint(event.to_dict())
+    if event.cffi_event.x86_regs.rip != event.data['target_vaddr']:
         # page hit, but we are not at the right address
-        # emulate this instruction and continue
-        return EventResponse.EMULATE
-    pname = dtb_to_pname(vmi, event.cffi_event.x86_regs.cr3)
-    print('At {}: {}'.format(event.data['symbol'], pname))
+        if event.data['sstep']:
+            # lift page permissions
+            print("lift page permissions")
+            vmi.clear_event(event.data['mem_event'])
+            # toogle singlestep ON
+            return EventResponse.TOGGLE_SINGLESTEP
+        else:
+            # emulate this instruction and continue
+            return EventResponse.EMULATE
+    else:
+        # hit
+        pname = dtb_to_pname(vmi, event.cffi_event.x86_regs.cr3)
+        print('At {}: {}'.format(event.data['symbol'], pname))
+    return EventResponse.EMULATE
+
+
+def cb_ss_event(vmi, event):
+    # out of the frame ?
+    if event.cffi_event.ss_event.gfn != event.data['target_gfn']:
+        print("reregister event")
+        # reregister mem event
+        vmi.register_event(event.data['mem_event'])
+        # toggle singlestep OFF
+        return EventResponse.TOGGLE_SINGLESTEP
 
 
 def main(args):
-    if len(args) != 3:
-        print('./memaccess-event.py <vm_name> <symbol>')
-        return 1
-
-    vm_name = args[1]
-    symbol = args[2]
+    vm_name = args['<vm_name>']
+    symbol = args['<symbol>']
+    sstep_enabled = args['--sstep']
 
     # register SIGINT
     signal.signal(signal.SIGINT, signal_handler)
 
     with Libvmi(vm_name, INIT_DOMAINNAME | INIT_EVENTS) as vmi:
         vaddr = vmi.translate_ksym2v(symbol)
-        print("symbol {} at {}".format(symbol, hex(vaddr)))
+        paddr = vmi.translate_kv2p(vaddr)
+        frame = paddr >> 12
+        print("symbol: {} vaddr: {} paddr: {} frame: {}".format(
+            symbol, hex(vaddr), hex(paddr), hex(frame)))
+
         user_data = {
             'symbol': symbol,
-            'symbol_addr': vaddr
+            'target_vaddr': vaddr,
+            'target_gfn': frame,
+            'mem_event': None,
+            'sstep': sstep_enabled
         }
-        paddr = vmi.translate_kv2p(vaddr)
-        page = paddr >> 12
-        print("paddr: {}".format(hex(paddr)))
-        print("page: {}".format(hex(page)))
-        mem_event = MemEvent(MemAccess.X, callback, gfn=page, data=user_data)
-        vmi.register_event(mem_event)
+        num_vcpus = vmi.get_num_vcpus()
+        ss_event = SingleStepEvent(range(num_vcpus), cb_ss_event,
+                                   data=user_data, enable=False)
+        mem_event = MemEvent(MemAccess.X, cb_mem_event, gfn=frame,
+                             data=user_data)
+        user_data['mem_event'] = mem_event
+        with pause(vmi):
+            vmi.register_event(ss_event)
+            vmi.register_event(mem_event)
         # listen
         while not interrupted:
-            print("Waiting for events")
+            print("Waiting for events ({})".format(vmi.are_events_pending()))
             vmi.listen(3000)
         print("Stop listening")
+        with pause(vmi):
+            vmi.clear_event(mem_event)
 
 
 if __name__ == '__main__':
-    ret = main(sys.argv)
+    args = docopt(__doc__)
+    ret = main(args)
     sys.exit(ret)
