96804ea48bdb66c874a99de53944cfa849e57b4d
Mathieu Tarral <mathieu.tarral@gmail.com>
2018-11-15 07:11:45 +0100

    export class Registers in __init__.py

--- libvmi/__init__.py
+++ libvmi/__init__.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import
 # public interface
 from .libvmi import INIT_DOMAINNAME, INIT_DOMAINID, INIT_EVENTS, INIT_SHM
 from .libvmi import (Libvmi, LibvmiError, VMIConfig, VMIMode, AccessContext,
-                     TranslateMechanism, X86Reg)
+                     TranslateMechanism, X86Reg, Registers)
 from .libvmi import VMIStatus, LibvmiInitError, PageMode
 from .libvmi import VMIArch, VMIOS, VMIWinVer
