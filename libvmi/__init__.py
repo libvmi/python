ba94d393e659d40904a262c2a42fd23539cc8447
Mathieu Tarral <mathieu.tarral@gmail.com>
2019-01-06 01:34:26 +0100

    remove INIT_SHM from __init__.py

--- libvmi/__init__.py
+++ libvmi/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 # public interface
-from .libvmi import INIT_DOMAINNAME, INIT_DOMAINID, INIT_EVENTS, INIT_SHM
+from .libvmi import INIT_DOMAINNAME, INIT_DOMAINID, INIT_EVENTS
 from .libvmi import (Libvmi, LibvmiError, VMIConfig, VMIMode, AccessContext,
                      TranslateMechanism, X86Reg, Registers)
 from .libvmi import VMIStatus, LibvmiInitError, PageMode
